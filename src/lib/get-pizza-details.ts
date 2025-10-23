61fc9abb7dcc5f15578520dd9d14bb0aa7158eed
romanchykov-webdev <romanchykov.webdev@gmail.com>
2025-10-23 18:41:30 +0200

    chore: обновлены текстовые строки на итальянском языке и исправлены отступы в компонентах

--- src/lib/get-pizza-details.ts
+++ src/lib/get-pizza-details.ts
@@ -1,19 +1,19 @@
-import { mapPizzaTypes, PizzaSize, PizzaType } from '@/constants/pizza';
-import { calcTotalPizzaPrice } from '@/lib/calc-total-pizza-price';
-import { Ingredient, ProductItem } from '@prisma/client';
+import { mapPizzaTypes, PizzaSize, PizzaType } from "@/constants/pizza";
+import { calcTotalPizzaPrice } from "@/lib/calc-total-pizza-price";
+import { Ingredient, ProductItem } from "@prisma/client";
 
 export const getPizzaDetails = (
-  type: PizzaType,
-  size: PizzaSize,
-  items: ProductItem[] = [],
-  ingredients: Ingredient[],
-  selectedIngredientsIds: Set<number>,
+	type: PizzaType,
+	size: PizzaSize,
+	items: ProductItem[] = [],
+	ingredients: Ingredient[],
+	selectedIngredientsIds: Set<number>,
 ) => {
-  const totalPrice = calcTotalPizzaPrice(type, size, items, ingredients, selectedIngredientsIds);
+	const totalPrice = calcTotalPizzaPrice(type, size, items, ingredients, selectedIngredientsIds);
 
-  const ingredientsList = `<strong>Ингридиенты:</strong>`;
+	const ingredientsList = `<strong>Ingredienti:</strong>`;
 
-  const textDetails = `${size} см ${mapPizzaTypes[type]} тесто. ${ingredientsList} ${ingredients.map(i => i.name).join(', ')} `;
+	const textDetails = `${size} cm ${mapPizzaTypes[type]} тесто. ${ingredientsList} ${ingredients.map((i) => i.name).join(", ")} `;
 
-  return { totalPrice, textDetails };
+	return { totalPrice, textDetails };
 };
